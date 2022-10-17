19ad8c3533998810d9a2901072893b2fb99c21b6
K1Pro <support@k1pro.net>
2022-10-17 22:42:42 +0200

    initializing version 2

---
+++ search.js
@@ -0,0 +1,7 @@
+'use strict';
+// import { ContactsURL } from './variables.js';
+let BartDate = new Date();
+// prettier-ignore
+const bartsMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec',];
+// prettier-ignore
+const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
