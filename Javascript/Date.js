68deff5cf40c16c32760239fe0f9602fca5f1dd5
K1Pro <support@k1pro.net>
2022-10-24 14:56:18 +0200

    refactoring Javascript into modules

---
+++ Javascript/Date.js
@@ -0,0 +1,9 @@
+'use strict';
+let BartDate = new Date();
+// prettier-ignore
+const bartsMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec',];
+// prettier-ignore
+const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
+const todaysDay = BartDate;
+console.log(todaysDay.toString());
+// const BartkaTestButton = document.getElementById('BartkaTestButton');
--- search.js
+++ search.js
@@ -1,7 +1,1 @@
-'use strict';
-// import { ContactsURL } from './variables.js';
-let BartDate = new Date();
-// prettier-ignore
-const bartsMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec',];
-// prettier-ignore
-const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
+// import { ContactsURL } from './date.js';
