c813cb6bc344822b20f836caef67da1649adccc9
K1Pro <support@k1pro.net>
2022-10-25 11:06:55 +0200

    calculating Calendar Dates Dynamically

--- Javascript/Date.js
+++ Javascript/Date.js
@@ -4,13 +4,46 @@ function myTimer() {
   if ('CalendarDate' in pageElements) {
     console.log('CalendarDate found');
     clearInterval(myInterval);
-    let BartDate = new Date();
+
+    let TodaysDate = new Date();
     // prettier-ignore
     const bartsMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec',];
     // prettier-ignore
     const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
+
     const CalendarHTML_Date = document.getElementById('CalendarDate');
+
+    CalendarHTML_Date.innerHTML = TodaysDate.toJSON().slice(0, 10);
     console.log(CalendarHTML_Date.innerHTML);
-    CalendarHTML_Date.innerText = BartDate.toJSON().slice(0, 10);
+
+    for (let rep = 1; rep < 29; rep++) {
+      let noOfDaysToPrevMonday = 0 - TodaysDate.getDay() - 7 + rep;
+      let CalendarDates = new Date(
+        Date.now() +
+          1000 /*sec*/ *
+            60 /*min*/ *
+            60 /*hour*/ *
+            24 /*day*/ *
+            noOfDaysToPrevMonday /*# of days*/
+      );
+      console.log(CalendarDates);
+      // targetDate.setDate(BartDate.getDate() - dayCounter + rep);
+      // console.log(targetDate);
+      // console.log(BartDate.getDay());
+      // console.log(dayCounter);
+      // dayCounterTwo = BartDate.getDate() - dayCounter + rep;
+      // console.log(dayCounterTwo);
+      // // console.log(`day${rep}`);
+      // document.getElementById(`day${rep}`).innerHTML = BartDate.toJSON().slice(
+      //   8,
+      //   10
+      // );
+      // .addEventListener('change', function (e) {
+      //   let ContactFieldID = this.id;
+      //   let ContactFieldValue = this.value;
+      //   updateContactInfo(id.value, ContactFieldID, ContactFieldValue);
+      // });
+      // }
+    }
   }
 }
