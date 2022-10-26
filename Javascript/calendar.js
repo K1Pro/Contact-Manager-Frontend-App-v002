fcb40ace2608df64efe7152acb343dc4554a60ed
K1Pro <support@k1pro.net>
2022-10-26 18:52:59 +0200

    Next and Previous Week buttons working

--- Javascript/calendar.js
+++ Javascript/calendar.js
@@ -5,31 +5,55 @@ function myTimer() {
     console.log('CalendarDate found');
     clearInterval(myInterval);
 
+    let chosenDate;
+    let daysInWeek = 7;
     let TodaysDate = new Date();
-
     const CalendarHTML_Date = document.getElementById('CalendarDate');
+    const CalendarHTML_PrevBtn = document.getElementById('CalendarPrev');
+    const CalendarHTML_NextBtn = document.getElementById('CalendarNext');
+
+    CalendarHTML_PrevBtn.addEventListener('click', function () {
+      daysInWeek = daysInWeek + 7;
+      calendarDatesFillIn(TodaysDate, daysInWeek);
+    });
+
+    CalendarHTML_NextBtn.addEventListener('click', function () {
+      daysInWeek = daysInWeek - 7;
+      calendarDatesFillIn(TodaysDate, daysInWeek);
+    });
+
     CalendarHTML_Date.innerHTML = TodaysDate.toJSON().slice(0, 10);
 
-    for (let rep = 1; rep < 29; rep++) {
-      let noOfDaysToPrevMonday = 0 - TodaysDate.getDay() - 7 + rep;
-
-      let CalendarDates = new Date(
-        Date.now() +
-          1000 /*sec*/ *
-            60 /*min*/ *
-            60 /*hour*/ *
-            24 /*day*/ *
-            noOfDaysToPrevMonday /*# of days*/
-      );
-
-      if (CalendarDates.toString() == TodaysDate.toString()) {
+    function calendarDatesFillIn(chosenDate, chosenWeek) {
+      for (let rep = 1; rep < 29; rep++) {
         document
           .getElementById(`day${rep}`)
-          .classList.add('calendarCurrentDay');
-      }
+          .classList.remove('calendarCurrentDay');
 
-      document.getElementById(`day${rep}`).innerHTML =
-        CalendarDates.toJSON().slice(5, 10);
+        let noOfDaysToPrevMonday = 0 - chosenDate.getDay() - chosenWeek + rep;
+
+        let CalendarDates = new Date(
+          Date.now() +
+            1000 /*sec*/ *
+              60 /*min*/ *
+              60 /*hour*/ *
+              24 /*day*/ *
+              noOfDaysToPrevMonday /*# of days*/
+        );
+
+        if (
+          CalendarDates.toJSON().slice(0, 10) ==
+          chosenDate.toJSON().slice(0, 10)
+        ) {
+          document
+            .getElementById(`day${rep}`)
+            .classList.add('calendarCurrentDay');
+        }
+
+        document.getElementById(`day${rep}`).innerHTML =
+          CalendarDates.toJSON().slice(5, 10);
+      }
     }
+    calendarDatesFillIn(TodaysDate, daysInWeek);
   }
 }
