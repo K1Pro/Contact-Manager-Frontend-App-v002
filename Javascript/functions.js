4c72a1457eae1d4b303bba18b750d6ecdf1a4521
K1Pro <support@k1pro.net>
2022-11-23 01:11:05 +0100

    successfully populated policy 1

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -27,36 +27,40 @@ function calendarDatesFillIn(chosenDate, chosenWeek) {
     }
     // This fills in the individual calendar date days
     // prettier-ignore
+    document.getElementById(`day${rep}`).setAttribute('data-day', `${CalendarDates.toJSON().slice(5, 10)}`);
+    // prettier-ignore
     document.getElementById(`day${rep}`).innerHTML = `${CalendarDates.toJSON().slice(5, 10)}`;
     document.getElementById(`day${rep}`).addEventListener('click', () => {
       CalendarHTML_Date.innerHTML = `${CalendarDates.toJSON().slice(0, 10)}`;
     });
-    // This fills in Calendar Dates with contacts that have a renewal or task scheduled on that day
-    console.log(
-      `${CalendarDates.toJSON().slice(5, 7)} - ${CalendarDates.toJSON().slice(
-        8,
-        10
-      )}`
-    );
   }
+
   getJSON(ContactsURL).then((data) => {
     for (const [key, value] of Object.entries(data.data.contacts)) {
       let Policy1RenewDate = `${value.Policy1RenewMonth}-${value.Policy1RenewDay}`;
-      console.log(Policy1RenewDate);
-    }
-    let mySpans = document.getElementsByClassName('day-hover');
-    for (let i = 0; i < mySpans.length; i++) {
-      if (mySpans[i].innerHTML == '11-18') {
-        console.log('found it');
+      // prettier-ignore
+      if (document.querySelector(`[data-day="${value.Policy1RenewMonth}-${value.Policy1RenewDay}"]`)) {
+        let p = document.createElement('div');
+        p.textContent = `${value.LastName}`;
+        p.classList.add("notCompleted");
+        p.classList.add("text-light");
+        // prettier-ignore
+        document.querySelector(`[data-day="${value.Policy1RenewMonth}-${value.Policy1RenewDay}"]`).appendChild(p);
       }
     }
+    // let mySpans = document.getElementsByClassName('day-hover');
+    // for (let i = 0; i < mySpans.length; i++) {
+    //   if (mySpans[i].innerHTML == '11-18') {
+
+    //   }
+    // }
     // for (let rep = 1; rep < 29; rep++) {
     //   let p = document.createElement('div');
     //   let b = document.createElement('div');
     //   document.getElementById(`day${rep}`).appendChild(p);
     //   document.getElementById(`day${rep}`).appendChild(b);
     // }
-    console.log(data.data.contacts);
+    // console.log(data.data.contacts);
   });
 }
 
