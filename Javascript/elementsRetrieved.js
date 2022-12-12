4577362908b408b7f38c8c955a2695ba9e1f1fa2
K1Pro <support@k1pro.net>
2022-12-12 22:00:16 +0100

    loading element async await improved check

--- Javascript/elementsRetrieved.js
+++ Javascript/elementsRetrieved.js
@@ -1,28 +1,28 @@
 function elementsrequired() {
   // vvv Start coding here for Retrieval Module vvv
 
+  // Contact Task Module Elements
+  ContactTaskList = document.getElementById('ContactTaskList');
+  contactTasksTextArea = document.getElementById('contactTasksTextArea');
+  createEventTime = document.getElementById('createEventTime');
+  createEvent = document.getElementById('createEvent');
+  // Side Panel Module Elements
+  contactSearch = document.getElementById('contactSearch');
+  contactsList = document.getElementById('contactsList');
+  IDinput = document.getElementById('_id');
+  ContactFields = document
+    .getElementById('ContactFields')
+    .querySelectorAll('*');
+  // Calendar Module Elements
   CalendarHTML_Date = document.getElementById('CalendarDate');
   CalendarHTML_PrevMonthBtn = document.getElementById('LastMonthButton');
   CalendarHTML_PrevWeekBtn = document.getElementById('LastWeekButton');
   CalendarHTML_NextWeekBtn = document.getElementById('NextWeekButton');
   CalendarHTML_NextMonthBtn = document.getElementById('NextMonthButton');
-  createEvent = document.getElementById('createEvent');
-  createEventTime = document.getElementById('createEventTime');
-  contactTasksTextArea = document.getElementById('contactTasksTextArea');
-  contactSearch = document.getElementById('contactSearch');
-  contactsList = document.getElementById('contactsList');
-  // TaskList = document.getElementById('TaskList');
-  ContactTaskList = document.getElementById('ContactTaskList');
-  IDinput = document.getElementById('_id');
-  // Retrieves contact inputs from side panel
-  ContactFields = document
-    .getElementById('ContactFields')
-    .querySelectorAll('*');
+  UniqueDays = document.getElementsByClassName('uniqueday');
   CalendarDates = document
     .getElementById('calendarDates')
     .querySelectorAll('*');
-  UniqueDays = document.getElementsByClassName('uniqueday');
-
   for (let rep = 0; rep < ContactFields.length; rep++) {
     // Side Panel inputs if changed will update database to these values
     let ContactFieldsIDs = ContactFields[rep].id;
@@ -59,6 +59,8 @@ function elementsrequired() {
 }
 // vvv All retrieved elements should be declared here vvv
 let DOMElements,
+  ContactTaskList,
+  IDinput,
   CalendarHTML_Date,
   CalendarHTML_PrevMonthBtn,
   CalendarHTML_PrevWeekBtn,
@@ -76,19 +78,21 @@ let DOMElements,
 // ^^^ All retrieved elements should be declared here ^^^
 
 // vvv This scans for all separate HTML Modules vvv
+
 async function isElementLoaded(selector) {
   while (document.querySelector(selector) === null) {
     await new Promise((resolve) => requestAnimationFrame(resolve));
   }
 }
-isElementLoaded(
-  '#CalendarHTMLModule' &&
-    '#ContactTasksHTMLModule' &&
-    // '#DailyTasksHTMLModule' &&
-    '#SidePanelHTMLModule' &&
-    '#EmailHTMLModule'
-).then(() => {
-  console.log('retrieved all modules {All DOM elements retrieved}');
-  elementsrequired();
+isElementLoaded('#CalendarHTMLModule').then(() => {
+  isElementLoaded('#ContactTasksHTMLModule').then(() => {
+    isElementLoaded('#EmailHTMLModule').then(() => {
+      isElementLoaded('#SidePanelHTMLModule').then(() => {
+        console.log('retrieved all modules {All DOM elements retrieved}');
+        elementsrequired();
+      });
+    });
+  });
 });
+
 // import { ContactsURL } from './date.js';
