93bf5f562cfc8ad33e53bcc375d59e089e5b948d
K1Pro <support@k1pro.net>
2023-02-18 01:00:27 +0100

    polishing up sidepanel updating

--- Global/elementsRetrieved.js
+++ Global/elementsRetrieved.js
@@ -41,50 +41,52 @@ function elementsRequired() {
         oldInputValue = this.value;
         return oldInputValue;
       });
-      document.getElementById(`${ContactFieldsIDs}`).addEventListener('change', function (e) {
-        saveOldValue(this);
-        let checkValid = document.getElementById(`${ContactFieldsIDs}`).checkValidity();
-        if (checkValid) {
-          if (_id.value) {
-            let checkEmptyFields = this.value;
-            if (!checkEmptyFields.replace(/\s/g, '').length) {
-              console.log('string only contains spaces, tabs or line breaks');
-              let updateThis = {
-                updateURL: `${srvrURL}${deleteEmptyFieldPath}${_id.value}`,
-                fetchMethod: 'DELETE',
-                value: '',
-              };
-              if (renewDateKeys.includes(this.id)) {
-                let changedInputMMDD = this.id.replace('Date', 'MMDD');
-                updateThis.key = changedInputMMDD;
-                updateDB(updateThis);
-                contactEditDate();
+      document.getElementById(`${ContactFieldsIDs}`).addEventListener('focusout', function (e) {
+        if (oldInputValue != this.value) {
+          saveOldValue(this);
+          let checkValid = document.getElementById(`${ContactFieldsIDs}`).checkValidity();
+          if (checkValid) {
+            if (_id.value) {
+              let checkEmptyFields = this.value;
+              if (!checkEmptyFields.replace(/\s/g, '').length) {
+                console.log('string only contains spaces, tabs or line breaks');
+                let updateThis = {
+                  updateURL: `${srvrURL}${deleteEmptyFieldPath}${_id.value}`,
+                  fetchMethod: 'DELETE',
+                  value: '',
+                };
+                if (renewDateKeys.includes(this.id)) {
+                  let changedInputMMDD = this.id.replace('Date', 'MMDD');
+                  updateThis.key = changedInputMMDD;
+                  updateDB(updateThis);
+                  contactEditDate();
+                } else {
+                  updateThis.key = this.id;
+                  updateDB(updateThis);
+                  contactEditDate();
+                }
               } else {
-                updateThis.key = this.id;
-                updateDB(updateThis);
-                contactEditDate();
+                let updateThis = {
+                  updateURL: `${srvrURL}/${_id.value}`,
+                  fetchMethod: 'PATCH',
+                };
+                if (renewDateKeys.includes(this.id)) {
+                  let changedInputMMDD = this.id.replace('Date', 'MMDD');
+                  let MMDD = this.value.slice(5, 10);
+                  updateThis.key = changedInputMMDD;
+                  updateThis.value = MMDD;
+                  updateDB(updateThis);
+                  contactEditDate();
+                } else {
+                  updateThis.key = this.id;
+                  updateThis.value = this.value;
+                  updateDB(updateThis);
+                  contactEditDate();
+                }
               }
             } else {
-              let updateThis = {
-                updateURL: `${srvrURL}/${_id.value}`,
-                fetchMethod: 'PATCH',
-              };
-              if (renewDateKeys.includes(this.id)) {
-                let changedInputMMDD = this.id.replace('Date', 'MMDD');
-                let MMDD = this.value.slice(5, 10);
-                updateThis.key = changedInputMMDD;
-                updateThis.value = MMDD;
-                updateDB(updateThis);
-                contactEditDate();
-              } else {
-                updateThis.key = this.id;
-                updateThis.value = this.value;
-                updateDB(updateThis);
-                contactEditDate();
-              }
+              snackbar('Please search for and choose a customer.');
             }
-          } else {
-            snackbar('Please search for and choose a customer.');
           }
         }
       });
