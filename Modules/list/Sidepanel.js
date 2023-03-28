0f2414efec4a30346489932ccefb9b5fd8191943
K1Pro <support@k1pro.net>
2023-03-28 14:53:49 +0200

    filtering working better

--- Modules/list/Sidepanel.js
+++ Modules/list/Sidepanel.js
@@ -8,14 +8,13 @@ function listSidePanelModule() {
   document.querySelectorAll('.contctKeysCheck').forEach((cntctCheckBox) => {
     cntctCheckBox.addEventListener('click', function (e) {
       localStorage.setItem(`BundleContactList-${e.target.id.slice(0, -5)}`, e.target.checked);
-      allContctKeysCheck = document.querySelectorAll('.contctKeysCheck');
       allContctKeysCheckedArray = [...allContctKeysCheck].filter((el) => el.checked == true);
+      valueArray = allContctKeysCheckedArray.map((element) => {
+        return element.id.slice(0, -5);
+      });
       allContctKeysInputArray = [...allContctKeysInput].filter((el) => el.value != '');
       inputArray = allContctKeysInputArray.map((element) => {
         return `&${element.id.slice(0, -5)}=${element.value}`;
-      });
-      valueArray = allContctKeysCheckedArray.map((element) => {
-        return element.id.slice(0, -5);
       });
 
       contactListHeaders.innerHTML = '';
@@ -45,11 +44,46 @@ function listSidePanelModule() {
     });
   });
 
-  // document.querySelectorAll('.contctKeysInput').forEach((cntctCheckBox) => {
-  //   cntctCheckBox.addEventListener('change', function (e) {
-  //     console.log(e.target.value);
-  //   });
-  // });
+  document.querySelectorAll('.contctKeysInput').forEach((cntctCheckBox) => {
+    cntctCheckBox.addEventListener('change', function (e) {
+      listCheckBox = document.getElementById(`${e.target.id.slice(0, -5)}Check`);
+      if (listCheckBox.checked == true) {
+        allContctKeysCheckedArray = [...allContctKeysCheck].filter((el) => el.checked == true);
+        valueArray = allContctKeysCheckedArray.map((element) => {
+          return element.id.slice(0, -5);
+        });
+        allContctKeysInputArray = [...allContctKeysInput].filter((el) => el.value != '');
+        inputArray = allContctKeysInputArray.map((element) => {
+          return `&${element.id.slice(0, -5)}=${element.value}`;
+        });
+
+        contactListHeaders.innerHTML = '';
+        contactList.innerHTML = '';
+        for (key in allContctKeysCheck) {
+          if (allContctKeysCheck[key].className) {
+            if (allContctKeysCheck[key].checked == true) {
+              tableHeader = document.createElement('th');
+              tableHeader.scope = 'row';
+              tableHeader.innerHTML = allContctKeysCheck[key].id.slice(0, -5);
+              contactListHeaders.appendChild(tableHeader);
+            }
+          }
+        }
+
+        getJSON(`${srvrURL}?fields=${valueArray.toString()}${inputArray.join('')}`).then((data) => {
+          data.data.contacts.forEach((contact) => {
+            tableRow = document.createElement('tr');
+            contactList.appendChild(tableRow);
+            valueArray.forEach((value) => {
+              tableData = document.createElement('td');
+              contact[value] ? (tableData.innerHTML = contact[value]) : (tableData.innerHTML = '');
+              tableRow.appendChild(tableData);
+            });
+          });
+        });
+      }
+    });
+  });
 
   // ^^^ End coding here for Calendar Module ^^^
 }
