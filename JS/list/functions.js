aa44ed7612282cbf85353e4e052e59cd6af374c3
K1Pro <support@k1pro.net>
2023-04-15 00:45:45 +0200

    added sorting function to table

--- JS/list/functions.js
+++ JS/list/functions.js
@@ -1,2 +1,39 @@
 console.log('retrieved contact list functions');
 ///////////////////////////////////////////////
+
+function populateListTable(sortFilter) {
+  rep = 0;
+  contactList.innerHTML = '';
+  contactListHeaders.innerHTML = '';
+  allContctKeysCheck.forEach((CheckBox) => {
+    storedCheckBoxValue = localStorage.getItem(`BundleContactList-${CheckBox.id.slice(0, -5)}`);
+    rep++;
+    if (storedCheckBoxValue == 'true') {
+      document.getElementById(CheckBox.id).checked = true;
+      tableHeader = document.createElement('th');
+      tableHeader.scope = 'row';
+      tableHeader.innerHTML = CheckBox.id.slice(0, -5);
+      contactListHeaders.appendChild(tableHeader);
+    }
+    if (rep == allContctKeysCheck.length) {
+      allContctKeysCheck = document.querySelectorAll('.contctKeysCheck');
+      allContctKeysCheckedArray = [...allContctKeysCheck].filter((el) => el.checked == true);
+      valueArray = [];
+      allContctKeysCheckedArray.forEach((element) => {
+        valueArray.push(element.id.slice(0, -5));
+      });
+
+      getJSON(`${srvrURL}?fields=${valueArray.toString()}&sort=${sortFilter}`).then((data) => {
+        data.data.contacts.forEach((contact) => {
+          tableRow = document.createElement('tr');
+          contactList.appendChild(tableRow);
+          valueArray.forEach((value) => {
+            tableData = document.createElement('td');
+            contact[value] ? (tableData.innerHTML = contact[value]) : (tableData.innerHTML = '');
+            tableRow.appendChild(tableData);
+          });
+        });
+      });
+    }
+  });
+}
--- Modules/list/Table.js
+++ Modules/list/Table.js
@@ -1,42 +1,12 @@
 function tableModule() {
   // vvv Start coding here for Calendar Module vvv
 
-  rep = 0;
-  allContctKeysCheck.forEach((CheckBox) => {
-    storedCheckBoxValue = localStorage.getItem(`BundleContactList-${CheckBox.id.slice(0, -5)}`);
-    rep++;
-    if (storedCheckBoxValue == 'true') {
-      document.getElementById(CheckBox.id).checked = true;
-      tableHeader = document.createElement('th');
-      tableHeader.scope = 'row';
-      tableHeader.innerHTML = CheckBox.id.slice(0, -5);
-      contactListHeaders.appendChild(tableHeader);
-    }
-    if (rep == allContctKeysCheck.length) {
-      allContctKeysCheck = document.querySelectorAll('.contctKeysCheck');
-      allContctKeysCheckedArray = [...allContctKeysCheck].filter((el) => el.checked == true);
-      valueArray = [];
-      allContctKeysCheckedArray.forEach((element) => {
-        valueArray.push(element.id.slice(0, -5));
-      });
-
-      getJSON(`${srvrURL}?fields=${valueArray.toString()}&sort=LastName`).then((data) => {
-        data.data.contacts.forEach((contact) => {
-          tableRow = document.createElement('tr');
-          contactList.appendChild(tableRow);
-          valueArray.forEach((value) => {
-            tableData = document.createElement('td');
-            contact[value] ? (tableData.innerHTML = contact[value]) : (tableData.innerHTML = '');
-            tableRow.appendChild(tableData);
-          });
-        });
-      });
-    }
-  });
+  populateListTable('LastName');
 
   [...contactListHeaders.querySelectorAll('*')].forEach((header) =>
     header.addEventListener('click', () => {
       console.log(header.innerHTML);
+      populateListTable(header.innerHTML);
     })
   );
   // ^^^ End coding here for Calendar Module ^^^
