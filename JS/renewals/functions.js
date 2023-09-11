1ec413b4bf125f1b5671a0832a99a2d3c6d1f47a
K1Pro <support@k1pro.net>
2023-09-11 18:27:12 +0200

    One more minor change for newly created contacts

--- JS/renewals/functions.js
+++ JS/renewals/functions.js
@@ -17,83 +17,85 @@ function populateListTable(contactData) {
       `${new Date(element['CreateDate']).addDays(60).toISOString().slice(0, 10).replaceAll('-', '')}`
     );
 
-    tableRow = document.createElement('tr');
-    contactList.appendChild(tableRow);
+    if (renewDateSort >= createDateSort) {
+      tableRow = document.createElement('tr');
+      contactList.appendChild(tableRow);
 
-    tableData1 = document.createElement('td');
-    element['FirstName'] ? (tableData1.innerHTML = element['FirstName']) : (tableData1.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData1.classList.add('table-danger');
-    tableRow.appendChild(tableData1);
+      tableData1 = document.createElement('td');
+      element['FirstName'] ? (tableData1.innerHTML = element['FirstName']) : (tableData1.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData1.classList.add('table-danger');
+      tableRow.appendChild(tableData1);
 
-    tableData2 = document.createElement('td');
-    element['LastName'] ? (tableData2.innerHTML = element['LastName']) : (tableData2.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData2.classList.add('table-danger');
-    tableRow.appendChild(tableData2);
+      tableData2 = document.createElement('td');
+      element['LastName'] ? (tableData2.innerHTML = element['LastName']) : (tableData2.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData2.classList.add('table-danger');
+      tableRow.appendChild(tableData2);
 
-    // tableData3 = document.createElement('td');
-    // element['SpouseName'] ? (tableData3.innerHTML = element['SpouseName']) : (tableData3.innerHTML = '');
-    // tableRow.appendChild(tableData3);
+      // tableData3 = document.createElement('td');
+      // element['SpouseName'] ? (tableData3.innerHTML = element['SpouseName']) : (tableData3.innerHTML = '');
+      // tableRow.appendChild(tableData3);
 
-    // tableData4 = document.createElement('td');
-    // element['SpouseLastName'] ? (tableData4.innerHTML = element['SpouseLastName']) : (tableData4.innerHTML = '');
-    // tableRow.appendChild(tableData4);
+      // tableData4 = document.createElement('td');
+      // element['SpouseLastName'] ? (tableData4.innerHTML = element['SpouseLastName']) : (tableData4.innerHTML = '');
+      // tableRow.appendChild(tableData4);
 
-    // tableData5 = document.createElement('td');
-    // element['Address'] ? (tableData5.innerHTML = element['Address']) : (tableData5.innerHTML = '');
-    // tableRow.appendChild(tableData5);
+      // tableData5 = document.createElement('td');
+      // element['Address'] ? (tableData5.innerHTML = element['Address']) : (tableData5.innerHTML = '');
+      // tableRow.appendChild(tableData5);
 
-    // tableData6 = document.createElement('td');
-    // element['Phone'] ? (tableData6.innerHTML = element['Phone']) : (tableData6.innerHTML = '');
-    // tableRow.appendChild(tableData6);
+      // tableData6 = document.createElement('td');
+      // element['Phone'] ? (tableData6.innerHTML = element['Phone']) : (tableData6.innerHTML = '');
+      // tableRow.appendChild(tableData6);
 
-    tableData12 = document.createElement('td');
-    element['RenewDate']
-      ? (tableData12.innerHTML = `${element['RenewDate'].replaceAll('-', '/')}/${year}`)
-      : (tableData12.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData12.classList.add('table-danger');
-    tableRow.appendChild(tableData12);
+      tableData12 = document.createElement('td');
+      element['RenewDate']
+        ? (tableData12.innerHTML = `${element['RenewDate'].replaceAll('-', '/')}/${year}`)
+        : (tableData12.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData12.classList.add('table-danger');
+      tableRow.appendChild(tableData12);
 
-    tableData10 = document.createElement('td');
-    element[`Policy${element['RenewNumber']}Type`]
-      ? (tableData10.innerHTML = element[`Policy${element['RenewNumber']}Type`])
-      : (tableData10.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData10.classList.add('table-danger');
-    tableRow.appendChild(tableData10);
+      tableData10 = document.createElement('td');
+      element[`Policy${element['RenewNumber']}Type`]
+        ? (tableData10.innerHTML = element[`Policy${element['RenewNumber']}Type`])
+        : (tableData10.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData10.classList.add('table-danger');
+      tableRow.appendChild(tableData10);
 
-    tableData11 = document.createElement('td');
-    element[`Policy${element['RenewNumber']}Number`]
-      ? (tableData11.innerHTML = element[`Policy${element['RenewNumber']}Number`])
-      : (tableData11.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData11.classList.add('table-danger');
-    tableRow.appendChild(tableData11);
+      tableData11 = document.createElement('td');
+      element[`Policy${element['RenewNumber']}Number`]
+        ? (tableData11.innerHTML = element[`Policy${element['RenewNumber']}Number`])
+        : (tableData11.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData11.classList.add('table-danger');
+      tableRow.appendChild(tableData11);
 
-    tableData8 = document.createElement('td');
-    element['LastEditedBy'] ? (tableData8.innerHTML = element['LastEditedBy']) : (tableData8.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData8.classList.add('table-danger');
-    tableRow.appendChild(tableData8);
+      tableData8 = document.createElement('td');
+      element['LastEditedBy'] ? (tableData8.innerHTML = element['LastEditedBy']) : (tableData8.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData8.classList.add('table-danger');
+      tableRow.appendChild(tableData8);
 
-    tableData9 = document.createElement('td');
-    element['LastReviewDate']
-      ? (tableData9.innerHTML = `${element['LastReviewDate'].slice(5, 10).replaceAll('-', '/')}/${element[
-          'LastReviewDate'
-        ].slice(0, 4)}`)
-      : (tableData9.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData9.classList.add('table-danger');
-    tableRow.appendChild(tableData9);
+      tableData9 = document.createElement('td');
+      element['LastReviewDate']
+        ? (tableData9.innerHTML = `${element['LastReviewDate'].slice(5, 10).replaceAll('-', '/')}/${element[
+            'LastReviewDate'
+          ].slice(0, 4)}`)
+        : (tableData9.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData9.classList.add('table-danger');
+      tableRow.appendChild(tableData9);
 
-    tableData7 = document.createElement('td');
-    element['Source'] ? (tableData7.innerHTML = element['Source']) : (tableData7.innerHTML = '');
-    if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData7.classList.add('table-danger');
-    tableRow.appendChild(tableData7);
+      tableData7 = document.createElement('td');
+      element['Source'] ? (tableData7.innerHTML = element['Source']) : (tableData7.innerHTML = '');
+      if (renewDateSort <= reviewDateSort) tableData7.classList.add('table-danger');
+      tableRow.appendChild(tableData7);
 
-    // tableData13 = document.createElement('td');
-    // element['RenewDate']
-    //   ? (tableData13.innerHTML = `Renew: ${renewDateSort} - Review: ${reviewDateSort} - Created: ${createDateSort}`)
-    //   : (tableData13.innerHTML = '');
-    // if (renewDateSort <= reviewDateSort || renewDateSort <= createDateSort) tableData13.classList.add('table-danger');
-    // tableRow.appendChild(tableData13);
+      // tableData13 = document.createElement('td');
+      // element['RenewDate']
+      //   ? (tableData13.innerHTML = `Renew: ${renewDateSort} - Review: ${reviewDateSort} - Created: ${createDateSort}`)
+      //   : (tableData13.innerHTML = '');
+      // if (renewDateSort <= reviewDateSort ) tableData13.classList.add('table-danger');
+      // tableRow.appendChild(tableData13);
 
-    // });
+      // });
+    }
   });
   // allContctKeysCheck = document.querySelectorAll('.contctKeysCheck');
   // allContctKeysCheck.forEach((CheckBox) => {
